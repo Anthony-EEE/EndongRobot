45a9a67f81220b89d6712463482ebc01cd43b276
endong-sun-anthony <75470873+Anthony-EEE@users.noreply.github.com>
2024-09-12 14:42:24 +0100

    add_new tools

--- tool/collect_rmse.py
+++ tool/collect_rmse.py
@@ -1,98 +1,84 @@
 import os
 import sys
-import json
-import math
-
-import pyttsx3
-import keyboard
 import numpy as np
-import time
-from sklearn.preprocessing import normalize
-
-test_starting_points = [280, -30, 0]
-
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import json
+# workspace: x: 0-350 y:-250-250 z:0
 # Setup the uArm
-sys.path.append('..')
+sys.path.append(os.path.join(os.path.dirname(__file__), 'c:/Users/11424/OneDrive/文档/uArm-Python-SDK'))
 from uarm.wrapper import SwiftAPI
-swift = SwiftAPI(port="COM8", callback_thread_pool_size=1)
-print(swift.get_device_info())
+swift = SwiftAPI(port="COM3", callback_thread_pool_size=1)
 
-def learn(states, actions, demo=5, lam=1e-6):
-    lam = lam
-    demo = 3
-    I = np.identity(demo)
-
-    learned_thetea = actions @ (states.T) @ (np.linalg.inv(states @ states.T + lam * I))
-    return learned_thetea
-
-
-def experiments(elements, target_skill, score, real_theta, states, real_actions):
-    print("states", states)
-    user_actions = []
+# def get_starting_points():
+#     starting_points = []
+#     for i in range(5):
+#         test_starting_points = np.ones(3)
+#         test_starting_points[0] = int(np.random.uniform(0, 350))
+#         test_starting_points[1] = int(np.random.uniform(-250, 250))
+#         test_starting_points[2] = 0
+#         starting_points.append(test_starting_points.tolist())
+#     return starting_points
 
 
-    user_actions = np.array(user_actions).T
-    learned_theta = learn(states, user_actions, demo=3)
-    el2 = np.linalg.norm(real_theta - learned_theta)
+def plot_trajectory(trajectory):
+    plt.figure()
+    for i in range(5):
+        traj = np.array(trajectory[i])
+        plt.plot(traj[:, 0], traj[:, 1])
+    plt.show()
 
-
-
-    # Student Demonstration
-    test_point = np.array([test_starting_points[0], test_starting_points[1] + 20]) # test_starting_points = [130, 60, 0]
-    swift.reset(x=test_starting_points[0], y=test_starting_points[1], z=50)
-    
-    x, y, z = swift.get_position(timeout=100000)
-    vel = 100  # 固定速度 mm/second
-    vel = 50
-    steps = 0
-    trajectory = []
-    while steps < 50:
-        trajectory.append([x, y])
-
-        delta_x, delta_y = learned_theta @ np.array([x, y, 1]).T
-        x += delta_x
-        y += delta_y
-        swift.set_position(x=x, y=y, z=50, speed=vel*60)
+def experiments(learned_theta, test_starting):
+    trajs = []
+    print(test_starting)
+    for i in range(5):
+        print(f"----------------Trajectory {i+1}--------------")
+        test_starting_points = test_starting[i]
+        # Student Demonstration
+        swift.reset(x=test_starting_points[0], y=test_starting_points[1], z=0)
+        # test_point = np.array([test_starting_points[0], test_starting_points[1] + 20]) # test_starting_points = [130, 60, 0]
         
         x, y, z = swift.get_position(timeout=100000)
-        steps += 1
-    diff = true_traj - trajectory
-    distances = np.linalg.norm(diff, axis=1)
-    E_RMSE = np.mean(distances)
+        vel = 1000
         
+        steps = 0
+        trajectory = []
+        while steps < 70:
+            trajectory.append([x, y])
 
+            delta_x, delta_y = learned_theta @ np.array([x, y, 1]).T
+            x += delta_x
+            y += delta_y
+            swift.set_position(x=x, y=y, z=0, speed=vel*60)
+            
+            x, y, z = swift.get_position(timeout=100000)
+            steps += 1
+        trajs.append(trajectory)
+    return trajs
+        
+def result_collection(thetas):
+    
+    for i in range(len(thetas)):
+        print(f"----------------Person {i+1}--------------")
+        result = defaultdict(list)
+        filename = rf'C:\Users\11424\OneDrive\文档\phd\TwoLinkRobotArmSimulation\tool\target_results_{i+1}.json'
+        # number of people
+        for j in range(len(thetas[0])):
+            print(f"----------------Phase {j+1}--------------")
+            trajs = experiments(thetas[i][j], starts)
+            result[f"phase_{j+1}"].append(trajs)
+                # Save to JSON file
+        with open(filename, 'w') as f:
+            json.dump(result, f)
 
-user_id = 'control_group_user_13'
+starts = [[142, -179, 0.0], [238, -160, 0.0], [215, 17, 0.0], [63, -216, 0.0], [105, 157, 0.0]]
 
-s1_desired_traj = np.load('skill_1_real_trajectory.npy')
-s2_desired_traj = np.load('skill_2_real_trajectory.npy')
+control_thetas =  np.load(rf'C:\Users\11424\Downloads\exp2_control_thetas.npy')
+target_thetas = np.load(rf'C:\Users\11424\Downloads\exp2_target_thetas.npy')
 
-
-# s1_state = np.array([[320, 220, 140],
-#                      [50, 170, -30],
-#                      [1, 1, 1]])
-s2_state = np.array([[280.0, 260., 130.],
-                     [60., -50., 70.],
-                     [1, 1, 1]])
-
-s1_state = np.array([[300, 220, 200],
-                     [60, 140, -70],
-                     [1, 1, 1]])
-
-s2_state_prime = np.array([ [320, 150, 250],
-                            [30, -20, 150],
-                            [1, 1, 1]])
-
-
-
-
-# # Real controller
-s1_theta = np.array([[-0.04, 0.08, 0.0],
-                       [0.08, -0.16, 0.0]])#点到斜线y=0.5x
-s2_theta = np.array([[-0.2, 0, 0.2*230],
-                       [0, -0.2, 0.2*110]]) 
-
-s1_actions = s1_theta @ s1_state
-s2_actions = s2_theta @ s2_state
-s2_actions_prime = s2_theta @ s2_state_prime
-
+# print(len(target_thetas))
+# print(len(target_thetas[0]))
+# trajs = experiments(control_thetas[0][0], starts)
+# print(trajs)
+# plot_trajectory(trajs)
+result_collection(target_thetas)
---
+++ tool/collect_traj.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Replace 'filename.npy' with the path to your .npy file
+data = np.load(rf'C:\Users\11424\Downloads\exp2_control_thetas.npy')
+print(np.shape(data))
+print(data)
